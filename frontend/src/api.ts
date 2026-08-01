0604270e8a40b1ad9e988807c7f21e91d73ca8a2
Hayder <hayder.alhatemi@gmail.com>
2026-08-01 19:46:05 +0300

    chore: use API environment variable

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/tasks",
+  baseURL: process.env.REACT_APP_API_URL,
 })
 
 export const getTasks = () => API.get("/")
